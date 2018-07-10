74511a9fba4f5a5b42785ce49c81586bcf30ed10
Aleksandra Ciesielska <aleksandra-ciesielska1@tlen.pl>
2018-07-10 16:02:29 +0200

    added form

--- src/main/java/pl/akademiakodu/AK_Spring_ExerciseMethod_BasicCalculatorTODO/controller/DescController.java
+++ src/main/java/pl/akademiakodu/AK_Spring_ExerciseMethod_BasicCalculatorTODO/controller/DescController.java
@@ -7,40 +7,60 @@ package pl.akademiakodu.AK_Spring_ExerciseMethod_BasicCalculatorTODO.controller;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.ModelMap;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.ResponseBody;
+import pl.akademiakodu.AK_Spring_ExerciseMethod_BasicCalculatorTODO.models.Person;
 
 import java.util.Random;
 import java.util.Set;
 import java.util.TreeSet;
 
 @Controller
-public class DescController
-{
+public class DescController {
 
     /**
      * Method responsible to get description template
+     *
      * @return template with image containing description of exercise
      */
     @GetMapping("/desc")
-    public String descGet()
-    {
+    public String descGet() {
         return "desc";
     }
 
     //@ResponseBody //@- adnotacje jaka to jest ścieżka w przeglądarce
     @GetMapping("/random") // będzie wywołana metoda random która zwraca string random od metody get rządanie adres URL
-    public String getRandom(ModelMap map)
-    {
+    public String getRandom(ModelMap map) {
         Set<Integer> lotto = new TreeSet<>();
         Random random = new Random();
 
-        while (lotto.size() !=6) //do jakiego pliku w html ma przejść string
+        while (lotto.size() != 6) //do jakiego pliku w html ma przejść string
         {
-            lotto.add(random.nextInt((49) +1 ));
+            lotto.add(random.nextInt((49) + 1));
         }
-        map.put("lotto",lotto);  //wrzucenie klucza i wartości
+        map.put("lotto", lotto);  //wrzucenie klucza i wartości
 
         return "random"; //spring do przekazywania mapy do html używa hash map
         // szuka pliku random.htmlktóry musi znajdować się w katalogu template
     }
-}
+
+    @GetMapping("/form")
+    public String getForm() {
+        return "form";
+    }
+
+    @GetMapping("/result")
+    public String result(@RequestParam String firstName,  //sprawia że to co wysłaliśmy z formularza zamienia nam to na firstname
+                         @RequestParam String lastName, ModelMap modelMap){
+
+        Person person = new Person(firstName, lastName);
+
+        modelMap.put("person",person);  //
+
+        return "person"; // jaki plik html ma być wyświetlony
+    }
+
+
+
+    }
+
